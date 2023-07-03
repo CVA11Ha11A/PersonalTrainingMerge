4503cb2795ba9d0704e2b098251702c54919b50f
USER-PC\USER <dad0221@naver.com>
2023-07-03 23:20:22 +0900

    MyPersonalMergeMain

---
+++ IfProjectName_001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfProjectName_001
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("여기는 메인에서 찍은 Console.WriteLine ");
+
+
+
+        }
+    }
+}
